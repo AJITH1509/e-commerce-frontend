36e0e86b3d9196993455af33f9b47d22b85dbd12
AJITH1509 <ajithmuthu799@gmail.com>
2023-05-15 21:59:10 +0530

    login flow added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
+import { LoginPage } from "./LoginPage";
+import { SignUpPage } from "./SignUpPage";
+import { ForgetPass } from "./Forget";
+import { VerifyOtp } from "./VerifyOtp";
+import { NewPassword } from "./newPassword";
 import { ProductList } from "./Product";
+import { Profile } from "./profile.jsx";
 import { Cart } from "./Cart";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
+        <Route exact path="/" element={<LoginPage />} />
+        <Route exact path="/signup" element={<SignUpPage />} />
+        <Route exact path="/login/forgetpassword" element={<ForgetPass />} />
+        <Route exact path="/verifyotp" element={<VerifyOtp />} />
+        <Route exact path="/setpassword" element={<NewPassword />} />
+        <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
